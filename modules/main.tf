a94c2078a7b30785843f765112d0c572d982f7d1
cleutonsampaio <cleutonsampaio@cloud.upwork.com>
2024-01-21 15:53:16 -0300

    teste

---
+++ modules/main.tf
@@ -0,0 +1,60 @@
+# Configure o provedor Kubernetes
+provider "kubernetes" {
+  config_path        = "~/.kube/config"  # Caminho para o arquivo de configuração do kubectl
+}
+
+# Defina um recurso Deployment para o Nginx
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx-tf-deployment"
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "nginx-tf"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx-tf"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nginx-tf"
+          image = "nginx:latest"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+# Defina um recurso Service NodePort para expor o Nginx
+resource "kubernetes_service" "nginx-service" {
+  metadata {
+    name = "nginx-tf-service"
+  }
+
+  spec {
+    selector = {
+      app = "nginx-tf"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+      node_port   = 30080
+    }
+
+    type = "NodePort"
+  }
+}
